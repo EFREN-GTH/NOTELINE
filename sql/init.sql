25ba071783e622126d9b936d1e7e83987925f539
EFREN-GTH <efrenojeda9@gmail.com>
2024-05-18 21:06:46 +0200

    Fix: db tables create names to lowecase

--- sql/init.sql
+++ sql/init.sql
@@ -2,14 +2,14 @@ CREATE DATABASE IF NOT EXISTS `notas_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 
 
 USE `notas_db`;
 
-CREATE TABLE IF NOT EXISTS `USERS` (
+CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
   `pass` varchar(100) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS `NOTES` (
+CREATE TABLE IF NOT EXISTS `notes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -17,5 +17,5 @@ CREATE TABLE IF NOT EXISTS `NOTES` (
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `email` (`email`),
-  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`email`) REFERENCES `USERS` (`email`)
+  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
